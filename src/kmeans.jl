9d146732d54248acc1b8d2c6d754c0d2657f4809
marubontan <marubontan@gmail.com>
2018-04-08 22:01:07 -0300

    change the structure of program

--- src/kmeans.jl
+++ src/kmeans.jl
@@ -2,9 +2,49 @@ using DataFrames
 using StatsBase
 include("dist.jl")
 
-type kMeans
+type kMeansResults
     x::DataFrames.DataFrame
     k::Int
+    estimatedClass::Array{Int}
+
+end
+
+function kMeans(data, k)
+    dataPointsNum = size(data, 1)
+    estimatedClass = Array{Int}(dataPointsNum)
+    sample!(1:kMeans.k, estimatedClass)
+
+    while true
+        # update representative points
+        representativePoints = []
+        for representativeIndex in 1:k
+            groupIndex = find(estimatedClass .== representativeIndex)
+            groupData = data[groupIndex, :]
+
+            # TODO: check the return type of colwise
+            representativePoint = [ valArray[1] for valArray in colwise(mean, groupData) ]
+            push!(representativePoints, representativePoint)
+        end
+
+        # update group belonging
+        tempEstimatedClass = Array{Int}(dataPointsNum)
+        for dataIndex in 1:dataPointsNum
+            dataPoint = Array(data[dataIndex, :])
+            distances = Array{Float64}(k)
+            for representativeIndex in 1:k
+                distances[representativeIndex] = calcDist(dataPoint, representativePoints[representativeIndex])
+            end
+
+            # TODO: check the existence of argmin
+            tempEstimatedClass[dataIndex] = sortperm(distances)[1]
+        end
+
+        if estimatedClass == tempEstimatedClass
+            break
+        end
+        estimatedClass = tempEstimatedClass
+    end
+    return kMeansResults(data, k, estimatedClass) 
 end
 
 function calcDist(sourcePoint::Array, destPoint::Array; method="euclidean")
@@ -19,42 +59,4 @@ function calcDist(sourcePoint::Array, destPoint::Array; method="euclidean")
     elseif method == "minkowski"
         return minkowski(sourcePoint, destPoint)
     end
-end
-
-function fit(kMeans::kMeans)
-    dataPointsNum = size(kMeans.x, 1)
-    estimatedClass = Array{Int}(dataPointsNum)
-    sample!(1:kMeans.k, estimatedClass)
-
-    while true
-        # update representative points
-        representativePoints = []
-        for representativeIndex in 1:kMeans.k
-            groupIndex = find(estimatedClass .== representativeIndex)
-            groupData = kMeans.x[groupIndex, :]
-
-            # TODO: check the return type of colwise
-            representativePoint = [ valArray[1] for valArray in colwise(mean, groupData) ]
-            push!(representativePoints, representativePoint)
-        end
-
-        # update group belonging
-        tempEstimatedClass = Array{Int}(dataPointsNum)
-        for dataIndex in 1:dataPointsNum
-            dataPoint = Array(kMeans.x[dataIndex, :])
-            distances = Array{Float64}(kMeans.k)
-            for representativeIndex in 1:kMeans.k
-                distances[representativeIndex] = calcDist(dataPoint, representativePoints[representativeIndex])
-            end
-
-            # TODO: check the existence of argmin
-            tempEstimatedClass[dataIndex] = sortperm(distances)[1]
-        end
-
-        if estimatedClass == tempEstimatedClass
-            break
-        end
-        estimatedClass = tempEstimatedClass
-    end
-    return estimatedClass
 end
