bb7df73d3f1de691569fb7986d7fd0dc2207902f
marubontan <marubontan@gmail.com>
2018-04-14 15:55:33 -0300

    add docstrings

--- src/dist.jl
+++ src/dist.jl
@@ -1,3 +1,18 @@
+"""
+    euclidean(sourcePoint, destPoint)
+
+Calculate the euclidean distance between sourcePoint and destPoint.
+
+# Arguments
+- `sourcePoint::Array`: Array to show the one point.
+- `destPoint::Array`: Array to show the one point.
+
+# Examples
+```julia_euclidean
+julia> euclidean([1,2,3], [2,3,4])
+1.7320508075688772
+```
+"""
 function euclidean(sourcePoint::Array, destPoint::Array)
     length(sourcePoint) == length(destPoint) || error("The lengths of source and destination points should be same.")
 
@@ -9,6 +24,21 @@ function euclidean(sourcePoint::Array, destPoint::Array)
     return dist
 end
 
+"""
+    minkowski(sourcePoint, destPoint)
+
+Calculate the minkowski distance between sourcePoint and destPoint.
+
+# Arguments
+- `sourcePoint::Array`: Array to show the one point.
+- `destPoint::Array`: Array to show the one point.
+
+# Examples
+```julia_minkowski
+julia> minkowski([1,2,3], [2,3,4])
+3
+```
+"""
 function minkowski(sourcePoint::Array, destPoint::Array)
     length(sourcePoint) == length(destPoint) || error("The lengths of source and destination points should be same.")
 
