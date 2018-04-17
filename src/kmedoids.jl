35e341923e82aad9de0194d61c5da18b0c9ee136
marubontan <marubontan@gmail.com>
2018-04-16 22:28:20 -0300

    add docstring

--- src/kmedoids.jl
+++ src/kmedoids.jl
@@ -8,7 +8,30 @@ type kMedoidsResults
     iterCount::Int
 end
 
-function kMedoids(distanceMatrix, k)
+"""
+    kMedoids(distanceMatrix, k)
+
+Do clustering to distanceMatrix with K-medoids algorithm
+
+# Argument
+- `distanceMatrix`: the distance matrix of data
+- `k::Int`: the number of clusters
+
+# Examples
+```julia-kMedoids
+julia> import Distances
+
+julia> distanceMatrix = Distances.pairwise(Distances.SqEuclidean(), [1 2 3;4 5 6])
+3×3 Array{Int64,2}:
+ 0  2  8
+ 2  0  2
+ 8  2  0
+
+julia> kMedoids(distanceMatrix, 2)
+kMedoidsResults([0 2 8; 2 0 2; 8 2 0], 2, [2, 1, 1], Array[[2, 1]], 1)
+```
+"""
+function kMedoids(distanceMatrix, k::Int)
     # initialize
     medoidsIndices = randomlyAssignMedoids(distanceMatrix, k)
 
