c227cf3bd08f79f43100312257067fef84b6af43
marubontan <marubontan@gmail.com>
2018-04-18 19:53:16 -0300

    test

--- src/kmeans.jl
+++ src/kmeans.jl
@@ -1,6 +1,7 @@
 using DataFrames
 using StatsBase
 include("dist.jl")
+include("utils.jl")
 
 type kMeansResults
     x::DataFrames.DataFrame
@@ -93,33 +94,4 @@ function updateGroupBelonging(data::DataFrame, dataPointsNum::Int, representativ
         cost += distances[classIndex] ^ 2
     end
     return tempEstimatedClass, cost
-end
-
-function judgeConvergence(estimatedClass, tempEstimatedClass)
-    return estimatedClass == tempEstimatedClass
-end
-
-"""
-    calcDist(sourcePoint, destPoint; method="euclidean")
-
-Calculate the distance between sourcePoint and destPoint by the method.
-
-# Argument
-- `sourcePoint::Array`: Array to show the one point.
-- `destPoint::Array`: Array to show the one point.
-- `method::String`: distance function for calculation. euclidean,minkowski.
-
-# Examples
-```julia-calcDist
-julia> calcDist([1,2,3], [2,3,4]; method="minkowski")
-3
-```
-"""
-function calcDist(sourcePoint::Array, destPoint::Array; method="euclidean")
-
-    if method == "euclidean"
-        return euclidean(sourcePoint, destPoint)
-    elseif method == "minkowski"
-        return minkowski(sourcePoint, destPoint)
-    end
 end
