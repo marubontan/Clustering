bea46c1c27b1a8143c7b4370ba7ac90dcc0cf1df
marubontan <marubontan@gmail.com>
2018-04-18 19:38:19 -0300

    extract common functions

---
+++ src/utils.jl
@@ -0,0 +1,32 @@
+function returnArgumentMin(targetArray::Array)
+    return sortperm(targetArray)[1]
+end
+
+function judgeConvergence(arrayA, arrayB)
+    return arrayA == arrayB
+end
+
+"""
+    calcDist(sourcePoint, destPoint; method="euclidean")
+
+Calculate the distance between sourcePoint and destPoint by the method.
+
+# Argument
+- `sourcePoint::Array`: Array to show the one point.
+- `destPoint::Array`: Array to show the  one point.
+- `method::String`: distance function for calculation. euclidean,minkowski.
+
+# Examples
+```julia-calcDist
+julia> calcDist([1,2,3], [2,3,4]; method="minkowski")
+3
+```
+"""
+function calcDist(sourcePoint::Array, destPoint::Array; method="euclidean")
+
+    if method == "euclidean"
+        return euclidean(sourcePoint, destPoint)
+    elseif method == "minkowski"
+        return minkowski(sourcePoint, destPoint)
+    end
+end
