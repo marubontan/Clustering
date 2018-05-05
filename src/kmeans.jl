885b8b042f6ef77b729084914865a78c99740052
marubontan <marubontan@gmail.com>
2018-05-05 18:37:03 -0300

    organize lines and add type info

--- src/kmeans.jl
+++ src/kmeans.jl
@@ -51,7 +51,10 @@ function kMeans(data::DataFrame, k::Int; initializer=nothing)
     while true
 
         # update
-        tempEstimatedClass, cost, nearestDist = updateGroupBelonging(data, dataPointsNum, centroids, k)
+        tempEstimatedClass, cost, nearestDist = updateGroupBelonging(data,
+                                                                     dataPointsNum,
+                                                                     centroids,
+                                                                     k)
 
         push!(costArray, cost)
 
@@ -83,7 +86,7 @@ function kMeans(data::DataFrame, k::Int; initializer=nothing)
 end
 
 
-function assignRandomKClass(dataPointsNum::Int, k)
+function assignRandomKClass(dataPointsNum::Int, k::Int)
     estimatedClass = Array{Int}(dataPointsNum)
     sample!(1:k, estimatedClass)
     return estimatedClass
@@ -114,7 +117,7 @@ function updateGroupBelonging(data::DataFrame,
     tempEstimatedClass = Array{Int}(dataPointsNum)
 
     cost = 0.0
-    distanceBetweenDataPointAndNearestCentroid = []
+    distanceBetweenDataPointAndNearestCentroid = Array{Float64}(dataPointsNum)
     for dataIndex in 1:dataPointsNum
         dataPoint = Array(data[dataIndex, :])
         distances = Array{Float64}(k)
@@ -122,7 +125,7 @@ function updateGroupBelonging(data::DataFrame,
             distances[centroidIndex] = calcDist(dataPoint, centroids[centroidIndex])
         end
 
-        push!(distanceBetweenDataPointAndNearestCentroid, minimum(distances))
+        distanceBetweenDataPointAndNearestCentroid[dataIndex] = minimum(distances)
         classIndex = indmin(distances)
         tempEstimatedClass[dataIndex] = classIndex
 
@@ -135,8 +138,8 @@ end
 
 function assignDataOnEmptyCluster(data::DataFrame,
                                   label::Array{Int},
-                                  centers::Array,
-                                  nearestDist::Array)
+                                  centers::Array{Array{Float64, 1}, 1},
+                                  nearestDist::Array{Float64, 1})
 
     emptyCluster = findEmptyCluster(label, centers)
     nearestDistProb = makeValuesProbabilistic(nearestDist)
@@ -152,24 +155,27 @@ end
 
 
 function findEmptyCluster(label::Array{Int}, centers::Array)
+
     emptyCluster = collect(setdiff(Set(1:length(centers)), Set(label)))
     return emptyCluster
 end
 
 
 function kMeansPlusPlus(data::DataFrame, k::Int)
+
     dataDict = dataFrameToDict(data)
     ind = randomlyChooseOneDataPoint(dataDict)
     distanceDict = calcDistBetweenCenterAndDataPoints(dataDict, ind)
 
     distanceProbDict = makeDictValueProbabilistic(distanceDict)
 
-    centroidsIndices = [ind]
-    for i in 1:k-1
+    centroidsIndices = Array{Int}(k)
+    centroidsIndices[1] = ind
+    for i in 2:k
         centroidsIndex = wrapperToStochasticallyPickUp(distanceProbDict, 1)[1]
-        push!(centroidsIndices, centroidsIndex)
+        centroidsIndices[i] = centroidsIndex
 
-        if i == k-1
+        if i == k
             break
         end
 
@@ -178,23 +184,27 @@ function kMeansPlusPlus(data::DataFrame, k::Int)
         distanceProbDict = makeDictValueProbabilistic(distanceDict)
     end
 
-    centroids = dataFrame2JaggedArray(data[Int.(centroidsIndices), :])
+    centroids = dataFrame2JaggedArray(data[centroidsIndices, :])
     return centroids
 end
 
 
-function randomlyChooseOneDataPoint(data::Dict)
+function randomlyChooseOneDataPoint(data::Dict{Int, Array{Float64, 1}})
     randomIndex = rand([k for k in keys(data)], 1)[1]
     return randomIndex
 end
 
 
-function wrapperToStochasticallyPickUp(data::Dict, n::Int)
-    index = []
-    probs = Float64[]
-    for pair in data
-        push!(index, pair[1])
-        push!(probs, pair[2])
+function wrapperToStochasticallyPickUp(data::Dict{Int, Float64},
+                                       n::Int)
+
+    len = length(keys(data))
+
+    index = Array{Int}(len)
+    probs = Array{Float64}(len)
+    for (i, pair) in enumerate(data)
+        index[i] = pair[1]
+        probs[i] = pair[2]
     end
 
     return stochasticallyPickUp(index, probs, n)
